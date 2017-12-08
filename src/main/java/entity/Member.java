af3212a1811a31ec98bec01d71ffd6a8fdbbfbb1
MalindaK <m.malinda@mubasher.net>
2017-12-08 23:48:04 +0530

    Removing unwanted classes

---
+++ src/main/java/entity/Member.java
@@ -0,0 +1,17 @@
+package entity;
+
+import java.util.Date;
+
+/**
+ * Created by MalindaK on 12/8/2017.
+ */
+public class Member {
+    private String firstName;
+    private String lastName;
+    private String fullName;
+    private Date birthday;
+    private String address;
+    private String phone;
+
+
+}
---
+++ src/main/java/entity/Student.java
@@ -0,0 +1,7 @@
+package entity;
+
+/**
+ * Created by MalindaK on 12/8/2017.
+ */
+public class Student {
+}
